255cd5e4847f5ee3427f928ff3c5d931c61db295
nar2p1nk <farrisisqandar@gmail.com>
2022-02-06 16:16:48 +0800

    completed

--- index.py
+++ index.py
@@ -2,5 +2,22 @@
 # example:
 # if the word is "racecar" then it would turn true since "racecar" backwards is still "racecar"
 # if the word is "hill" then it would return false since "hill" backwards is "llih"
+ 
+import argparse
 
+parser = argparse.ArgumentParser(description='enter a word')
 
+parser.add_argument('word',type=str)
+
+args = parser.parse_args()
+
+# main function
+
+def palindroma(word):
+    reversedWord = ''.join(reversed(word))
+    if word == reversedWord:
+        print('this is a palindrome')
+    else:
+        print('this is not a palindrome')
+
+palindroma(args.word)
